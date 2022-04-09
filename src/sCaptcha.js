c0ab9ba3949c93be6aa4555d331b164800ae982c
Mauro Baladés <mauro.balades@tutanota.com>
2022-04-10 00:16:36 +0200

    added main structure of the captcha

--- src/sCaptcha.js
+++ src/sCaptcha.js
@@ -1,7 +1,26 @@
 var sCaptcha = {
-  captchaAction: () => {},
+  id: "sCaptcha",
 
-  captchaElement: () => {},
+  sCaptcha__icon: `<div class="sCaptcha__icon"></div>`,
+  sCaptcha__button: `<div class="sCaptcha__button"></div>`,
+  sCaptcha__text: `<div class="sCaptcha__text"></div>`,
 
-  generateCaptcha: () => {}
+  captchaAction() {},
+
+  captchaElement() {
+    return `
+      <div id="${this.id}">
+        ${this.sCaptcha__icon}
+        <div class="sCaptcha__wrapper">
+          ${this.sCaptcha__text}
+          ${this.sCaptcha__button}
+        </div>
+      </div>`;
+  },
+
+  generateCaptcha() {
+    document.write(this.captchaElement());
+  }
 };
+
+sCaptcha.generateCaptcha();
